ba990b9080c440c8a56a0897ff1744dd7de6fac4
aryakumar.george <hyL!Ufp9D9czQb7>
2024-10-17 15:42:15 +0100

    add request/response dtos

--- src/main/java/uk/go/hm/icb/dto/ICBRequest.java
+++ src/main/java/uk/go/hm/icb/dto/ICBRequest.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Data
 public class ICBRequest {
     private final List<SearchSource> searchSources;
-    private final List<SearchIDType> searchIDTypes;
+    private final List<SearchIdentifiers> searchIDTypes;
     private final SearchBioDetails searchBioDetails;
     private final SearchAddress searchAddress;
 }
