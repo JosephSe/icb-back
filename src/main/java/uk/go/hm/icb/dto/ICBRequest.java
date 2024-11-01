4c91d2e68cf342e2ae5336504900c0341def66b9
Joseph Sebastian <Joseph.Sebastian@digital.homeoffice.gov.uk>
2024-11-01 14:02:18 +0000

    logic and tests added

--- src/main/java/uk/go/hm/icb/dto/ICBRequest.java
+++ src/main/java/uk/go/hm/icb/dto/ICBRequest.java
@@ -1,10 +1,12 @@
 package uk.go.hm.icb.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@Builder
 public class ICBRequest {
     private final List<SearchSource> searchSources;
     private final List<SearchIdentifiers> searchIDTypes;
